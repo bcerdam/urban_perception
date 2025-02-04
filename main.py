ea63b0c0da0ef8cd4c159dea86592449b040e120
bcerdam <bcmardini@gmail.com>
2025-02-04 14:11:59 -0300

    torch error

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def train_one_epoch(epoch_index, num_epochs, train_dataloader, device, optimizer
     running_loss = 0.
     last_loss = 0.
 
-    print(train_dataloader, device, optimizer, model)
     for batch_idx, batch in enumerate(train_dataloader):
+        print(batch_idx)
         left_images_batch = batch[0].to(device)
         right_images_batch = batch[1].to(device)
         labels_batch = batch[2].unsqueeze(dim=1).to(device)
