2c25d46b5512463371d851c0734235a1ebbde392
bcerdam <bcmardini@gmail.com>
2025-02-05 12:44:24 -0300

    metrics

--- main.py
+++ main.py
@@ -9,40 +9,6 @@ from RawFeat import RawFeat
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-# # CUDA
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#
-# # Transforms
-# transform = transforms.Compose([
-#     transforms.Lambda(lambda img: crop_from_bottom(img, 25)),
-#     transforms.Resize((224, 224), antialias=True)
-# ])
-#
-# # Dataset
-# pp2 = PP2Dataset('data/cleaned_votes.tsv', 'data/cleaned_locations.tsv',
-#                  'data/places.tsv', 'data/images', transform=transform)
-#
-# # Visualization
-# # test = pp2[0]
-# # plot_tuple(test)
-#
-# # Split
-# train, validation = torch.utils.data.random_split(pp2, [int(len(pp2)*0.75)+1, int(len(pp2)*0.25)])
-#
-# # Dataloaders
-# train_dataloader = DataLoader(train, batch_size=64, shuffle=True)
-# validation_dataloader = DataLoader(validation, batch_size=64, shuffle=True)
-#
-# # Training loop
-#
-# # Model
-# model = resnet50(weights='DEFAULT')
-# model = RawFeat(model).to(device)
-#
-# # Optimizer (Temporary, paper does not specify which to use)
-# optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
-
 def train_one_epoch(epoch_index, num_epochs, train_dataloader, device, optimizer, model):
     running_loss = 0.
     last_loss = 0.
@@ -67,18 +33,17 @@ def train_one_epoch(epoch_index, num_epochs, train_dataloader, device, optimizer
         running_loss += loss_batch.item()
         last_loss = running_loss / (batch_idx + 1)  # loss per batch
 
-        # Print loss every 1000 batches
-        if batch_idx % 1000 == 0:
-            avg_loss = running_loss / (batch_idx+1)  # Average loss so far
-            print(f'Epoch: {epoch_index}/{num_epochs}, Batch: {batch_idx}/{len(train_dataloader)}, Loss: {avg_loss:.4f}')
-
-    print(f'LAST EPOCH UPDATE: Epoch: {epoch_index}/{num_epochs}, Train Loss: {last_loss}')
+    print(f'Epoch: {epoch_index}/{num_epochs}, Train Loss: {last_loss}')
     return last_loss
 
 def validate_model(epoch_index, num_epochs, validation_dataloader, device, model):
     model.eval()
     total_loss = 0.
     num_batches = 0
+
+    correct_predictions = 0
+    total_samples = 0
+    similarity_threshold = 0.05  # Tolerance for label 0
 
     with torch.no_grad():
         for batch in validation_dataloader:
@@ -90,12 +55,23 @@ def validate_model(epoch_index, num_epochs, validation_dataloader, device, model
             right_scores_batch = model.forward(right_images_batch, right_images_batch.shape[0])
 
             loss_batch = utils.loss(left_scores_batch, right_scores_batch, labels_batch, 1, 1, device)
-
             total_loss += loss_batch.item()
             num_batches += 1
 
+            left_scores = left_scores_batch.squeeze()
+            right_scores = right_scores_batch.squeeze()
+            predictions = torch.zeros_like(labels_batch.squeeze())
+            predictions[left_scores > right_scores] = -1
+            predictions[left_scores < right_scores] = 1
+            predictions[torch.abs(left_scores - right_scores) < similarity_threshold] = 0
+
+            correct_predictions += (predictions == labels_batch.squeeze()).sum().item()
+            total_samples += labels_batch.squeeze().shape[0]
+
     avg_loss = total_loss / num_batches
-    print(f'LAST EPOCH UPDATE: Epoch: {epoch_index}/{num_epochs}, Validation Loss: {avg_loss}')
+    accuracy = (correct_predictions / total_samples) * 100
+    print(f'Epoch: {epoch_index}/{num_epochs}, Validation Loss: {avg_loss}, Accuracy: {accuracy:.2f}%')
+
     return avg_loss
 
 def train_model(num_epochs, train_dataloader, validation_dataloader, device, optimizer, model):
