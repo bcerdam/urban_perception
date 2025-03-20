a47701a93faa90db77d8331eee9824ca2199a1da
bcerdam <bcmardini@gmail.com>
2025-03-20 20:00:45 -0300

    cluster

--- main.py
+++ main.py
@@ -95,6 +95,8 @@ if __name__ == "__main__":
 
     # CUDA
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    print(device)
 
     # Transforms
     transform = transforms.Compose([
