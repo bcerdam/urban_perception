f62d50116f78e73420bdb9174938ca8ce41f5a88
bcerdam <bcmardini@gmail.com>
2025-03-21 19:45:54 -0300

    sample size

--- custom_datasets.py
+++ custom_datasets.py
@@ -7,7 +7,7 @@ pd.set_option('future.no_silent_downcasting', True)
 
 class PP2Dataset(Dataset):
     def __init__(self, votes_path, locations_path, places_path, img_dir, transform=None):
-        self.votes_df = pd.read_csv(votes_path, sep='\t').sample(n=25000, random_state=42)
+        self.votes_df = pd.read_csv(votes_path, sep='\t').sample(n=5000, random_state=42)
         # self.votes_df = pd.read_csv(votes_path, sep='\t')
         self.locations_df = pd.read_csv(locations_path, sep='\t')
         self.places_df = pd.read_csv(places_path, sep='\t')
