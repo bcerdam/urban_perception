efb3c4bae030a723a03e982709c9409262292790
bcerdam <bcmardini@gmail.com>
2025-03-22 17:09:02 -0300

    model/2

--- RawFeat.py
+++ RawFeat.py
@@ -10,19 +10,19 @@ class RawFeat(nn.Module):
         for param in self.resnet50_4f.parameters():
             param.requires_grad = False
 
-        self.fc1 = nn.Linear(2048, 4096)
-        # self.fc1 = nn.Linear(512, 8)
-        self.fc2 = nn.Linear(4096, 1)
-        # self.fc2 = nn.Linear(8, 1)
+        # self.fc1 = nn.Linear(2048, 4096)
+        self.fc1 = nn.Linear(512, 1024)
+        # self.fc2 = nn.Linear(4096, 1)
+        self.fc2 = nn.Linear(1024, 1)
         self.relu = nn.ReLU()
-        self.drop = nn.Dropout(0.3)
-        # self.drop = nn.Dropout(0.75)
+        # self.drop = nn.Dropout(0.3)
+        self.drop = nn.Dropout(0.5)
 
     def forward(self, image, batch_size):
 
         image_features = self.resnet50_4f(image)
-        image_features = image_features.view(batch_size, 2048)
-        # image_features = image_features.view(batch_size, 512)
+        # image_features = image_features.view(batch_size, 2048)
+        image_features = image_features.view(batch_size, 512)
 
         x = self.fc1(image_features)
         x = self.relu(x)
