c18a057a0e59d972148b2b7d4f3e7d0690f1d4ce
bcerdam <bcmardini@gmail.com>
2025-03-23 22:07:13 -0300

    lr

--- main.py
+++ main.py
@@ -164,8 +164,7 @@ if __name__ == "__main__":
     # model = resnet18(weights='DEFAULT')
     # model = RawFeat(model).to(device)
 
-    # optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
-    optimizer = torch.optim.Adam(model.parameters(), lr=10**-9, weight_decay=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-4)
     # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     train_model(NUM_EPOCHS, train_dataloader, validation_dataloader, device, optimizer, model)
 
