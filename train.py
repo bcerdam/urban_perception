a6ef75ae58f64aa1402ae3a163947b97089da42a
bcm <bcmardini@gmail.com>
2025-03-29 15:48:23 -0300

    sample

--- train.py
+++ train.py
@@ -1,7 +1,19 @@
 import utils
 import torch
 import os
+from torch.utils.data import Dataset
+# from torch.utils.data import Subset
 
+
+class SampledDataset(Dataset):
+    def __init__(self, data):
+        self.data = data  # Store sampled data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]  # Return sample
 
 def train_one_epoch(epoch_index, train_dataloader, device, optimizer, model, m_w, m_t, similarity_threshold, pp2_train):
     model.train()
@@ -22,5 +34,11 @@ def train_one_epoch(epoch_index, train_dataloader, device, optimizer, model, m_w
     os.makedirs('model_checkpoints', exist_ok=True)
     checkpoint_path = os.path.join('model_checkpoints', f"model_epoch_{epoch_index}.pth")
     torch.save(model.state_dict(), checkpoint_path)
-    utils.metrics(pp2_train, model, m_w, m_t, similarity_threshold, checkpoint_path, epoch_index)
+
+    dataset_size = len(pp2_train)
+    indices = torch.randperm(dataset_size)[:100]  # Randomly select 100 samples
+    sampled_data = [pp2_train[int(i)] for i in indices]
+    sampled_pp2_train = SampledDataset(sampled_data)
+
+    utils.metrics(sampled_pp2_train, model, m_w, m_t, similarity_threshold, checkpoint_path, epoch_index)
     return checkpoint_path
