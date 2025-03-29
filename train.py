5f1c08b2773082f67ba03582b33fc90816f090c7
bcerdam <bcmardini@gmail.com>
2025-03-29 19:47:55 -0300

    sample size

--- train.py
+++ train.py
@@ -35,10 +35,11 @@ def train_one_epoch(epoch_index, train_dataloader, device, optimizer, model, m_w
     checkpoint_path = os.path.join('model_checkpoints', f"model_epoch_{epoch_index}.pth")
     torch.save(model.state_dict(), checkpoint_path)
 
-    dataset_size = len(pp2_train)
-    indices = torch.randperm(dataset_size)[:100]  # Randomly select 100 samples
-    sampled_data = [pp2_train[int(i)] for i in indices]
-    sampled_pp2_train = SampledDataset(sampled_data)
-
-    utils.metrics(sampled_pp2_train, model, m_w, m_t, similarity_threshold, checkpoint_path, epoch_index)
+    # dataset_size = len(pp2_train)
+    # indices = torch.randperm(dataset_size)[:100]  # Randomly select 100 samples
+    # sampled_data = [pp2_train[int(i)] for i in indices]
+    # sampled_pp2_train = SampledDataset(sampled_data)
+    #
+    # utils.metrics(sampled_pp2_train, model, m_w, m_t, similarity_threshold, checkpoint_path, epoch_index)
+    utils.metrics(pp2_train, model, m_w, m_t, similarity_threshold, checkpoint_path, epoch_index)
     return checkpoint_path
